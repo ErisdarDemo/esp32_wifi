8b1dcc4452f7e2b3048cf721516b1daa287593e2
Justin Reina <justinmreina@gmail.com>
2025-03-07 17:13:48 -0800

    (C)    apply style

--- main/main.c
+++ main/main.c
@@ -1,99 +1,145 @@
-/* WiFi station Example
+/**************************************************************************************************/
+/** @file     station_example_main.c
+ *  @brief    WiFi station Example
+ *  @details  This example code is in the Public Domain (or CC0 licensed, at your option.)
+ *
+ *  @author   Justin Reina, Firmware Engineer
+ *  @created  1/15/25
+ *  @last rev 1/15/25
+ *
+ *
+ *  @notes    x
+ *
+ *  @section    Opens
+ *      none current
+ *
+ *  @section    Legal Disclaimer
+ *      © 2025 Justin Reina, All rights reserved. All contents of this source file and/or any other
+ *      related source files are the explicit property of Justin Reina. Do not distribute.
+ *      Do not copy.
+ */
+/**************************************************************************************************/
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+//************************************************************************************************//
+//                                            INCLUDES                                            //
+//************************************************************************************************//
+
+//Standard Library Includes
 #include <string.h>
+
+//FreeRTOS Includes
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
+
+//Library Includes
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+//SDK Includes
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
-#include "nvs_flash.h"
 
-#include "lwip/err.h"
-#include "lwip/sys.h"
 
-/* The examples use WiFi configuration that you can set via project configuration menu
+//************************************************************************************************//
+//                                        DEFINITIONS & TYPES                                     //
+//************************************************************************************************//
 
-   If you'd rather not, just change the below entries to strings with the config you want - ie 
-   #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-#define EXAMPLE_ESP_WIFI_SSID      "MCC Corp"
-#define EXAMPLE_ESP_WIFI_PASS      "Metro2016"
-#define EXAMPLE_ESP_MAXIMUM_RETRY  (5)
+//-----------------------------------------  Definitions -----------------------------------------//
 
-#if CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK
-#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
-#define EXAMPLE_H2E_IDENTIFIER ""
-#elif CONFIG_ESP_WPA3_SAE_PWE_HASH_TO_ELEMENT
-#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HASH_TO_ELEMENT
-#define EXAMPLE_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
-#elif CONFIG_ESP_WPA3_SAE_PWE_BOTH
-#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_BOTH
-#define EXAMPLE_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
-#endif
-#if CONFIG_ESP_WIFI_AUTH_OPEN
-#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
-#elif CONFIG_ESP_WIFI_AUTH_WEP
-#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WEP
-#elif CONFIG_ESP_WIFI_AUTH_WPA_PSK
-#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
-#elif CONFIG_ESP_WIFI_AUTH_WPA2_PSK
-#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
-#elif CONFIG_ESP_WIFI_AUTH_WPA_WPA2_PSK
-#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_WPA2_PSK
-#elif CONFIG_ESP_WIFI_AUTH_WPA3_PSK
-#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA3_PSK
-#elif CONFIG_ESP_WIFI_AUTH_WPA2_WPA3_PSK
-#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_WPA3_PSK
-#elif CONFIG_ESP_WIFI_AUTH_WAPI_PSK
-#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WAPI_PSK
-#endif
+//Network Credentials
+#define EXAMPLE_ESP_WIFI_SSID      			"MCC Corp"
+#define EXAMPLE_ESP_WIFI_PASS      			"Metro2016"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  			(5)
 
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t s_wifi_event_group;
+//Network Config
+#define ESP_WIFI_SAE_MODE 					WPA3_SAE_PWE_BOTH
+#define EXAMPLE_H2E_IDENTIFIER 				CONFIG_ESP_WIFI_PW_ID
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD 	WIFI_AUTH_WPA2_PSK
 
 /* The event group allows multiple bits for each event, but we only care about two events:
  * - we are connected to the AP with an IP
  * - we failed to connect after the maximum amount of retries */
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
+#define WIFI_CONNECTED_BIT 					BIT0
+#define WIFI_FAIL_BIT      					BIT1
 
+
+//************************************************************************************************//
+//                                            VARIABLES                                           //
+//************************************************************************************************//
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+//Project variables
 static const char *TAG = "wifi station";
 
 static int s_retry_num = 0;
 
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
-{
+//************************************************************************************************//
+//                                         PRIVATE FUNCTIONS                                      //
+//************************************************************************************************//
+
+/**************************************************************************************************/
+/** @fcn        static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+ *  @brief      x
+ *  @details    x
+ *
+ *  @param    [in]  (void *) arg - x
+ *  @param    [in[  (esp_event_base_t) event_base - x
+ *  @param    [in]  (int32_t) event_id - x
+ *  @param    [in]  (void *) event_data - x
+ */
+/**************************************************************************************************/
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+	
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		
         esp_wifi_connect();
+        
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		
         if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+			
             esp_wifi_connect();
+            
             s_retry_num++;
+            
             ESP_LOGI(TAG, "retry to connect to the AP");
         } else {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
+        
         ESP_LOGI(TAG,"connect to the AP fail");
+        
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        
         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        
         s_retry_num = 0;
+        
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
+    
+    return;
 }
 
-void wifi_init_sta(void)
-{
+
+/**************************************************************************************************/
+/** @fcn        void wifi_init_sta(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+void wifi_init_sta(void) {
+	
     s_wifi_event_group = xEventGroupCreate();
 
     ESP_ERROR_CHECK(esp_netif_init());
@@ -132,7 +178,9 @@ void wifi_init_sta(void)
         },
     };
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    
     ESP_ERROR_CHECK(esp_wifi_start() );
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
@@ -140,26 +188,33 @@ void wifi_init_sta(void)
     /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
      * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
+							               WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+								           pdFALSE,
+								           pdFALSE,
+								           portMAX_DELAY);
 
     /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
      * happened. */
     if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
     } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
     } else {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
+    
+    return;
 }
 
-void app_main(void)
-{
+
+/**************************************************************************************************/
+/** @fcn        void app_main(void)
+ *  @brief      x
+ *  @details    x
+ */
+/**************************************************************************************************/
+void app_main(void) {
+	
     //Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -170,4 +225,6 @@ void app_main(void)
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
+    
+    return;
 }
